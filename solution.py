98eef16f8844daf5649621c472ce57c1042c54f1
Minhang Xie <minhangxie97@gmail.com>
2022-06-04 19:02:32 -0700

    moving paddles: manual only

--- solution.py
+++ solution.py
@@ -10,22 +10,68 @@ FPS = 60
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
-def render(win):
+PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
+
+class Paddle:
+
+    COLOR = WHITE
+    VELO = 4
+
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def render_paddle(self, win):
+        pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.width, self.height))
+
+    def move(self, up=True):
+        if up:
+            self.y -= self.VELO
+        else:
+            self.y += self.VELO
+
+
+def render_game(win, paddles):
     win.fill(BLACK)
+
+    for paddle in paddles:
+        paddle.render_paddle(win)
+
     pygame.display.update()
+
+def handle_paddle_move(keys, left_paddle, right_paddle):
+    if keys[pygame.K_w]:
+        left_paddle.move(up=True)
+    if keys[pygame.K_s]:
+        left_paddle.move(up=False)
+
+    if keys[pygame.K_UP]:
+        right_paddle.move(up=True)
+    if keys[pygame.K_DOWN]:
+        right_paddle.move(up=False)
+
 
 def main():
     running = True
     clock = pygame.time.Clock()
 
+    left_paddle = Paddle(10, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
+    right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
+
     while running:
         clock.tick(FPS)
-        render(WINDOW)
+        render_game(WINDOW, [left_paddle, right_paddle])
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
                 break
+
+        keys = pygame.key.get_pressed()
+        handle_paddle_move(keys, left_paddle, right_paddle)
+
     
     pygame.quit()
 
